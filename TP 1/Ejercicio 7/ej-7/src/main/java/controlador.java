c09ea67536c4c6606f9d76685562fbfbb40f2787
leoduville <71992403+leoduville@users.noreply.github.com>
2023-04-05 23:37:00 -0300

    "actualizo"

--- TP 1/Ejercicio 7/ej-7/src/main/java/controlador.java
+++ TP 1/Ejercicio 7/ej-7/src/main/java/controlador.java
@@ -7,13 +7,12 @@ import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.command.ExecStartResultCallback;
 import com.google.gson.Gson;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import java.net.http.HttpResponse;
 
 
 @RestController
@@ -73,9 +72,6 @@ public class controlador {
     return new ResponseEntity<String>(response, HttpStatus.OK);
   }
 
-  public static void main(String[] args) {
-    SpringApplication.run(controlador.class, args);
-  }
 
 }
 
