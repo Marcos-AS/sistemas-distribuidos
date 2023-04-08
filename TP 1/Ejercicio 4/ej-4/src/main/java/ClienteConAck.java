74bc14ddfbeecc2526b56283928f0cc359b49f83
leoduville <71992403+leoduville@users.noreply.github.com>
2023-04-08 12:30:09 -0300

    "ej 4 creo que anda"

--- TP 1/Ejercicio 4/ej-4/src/main/java/ClienteConAck.java
+++ TP 1/Ejercicio 4/ej-4/src/main/java/ClienteConAck.java
@@ -5,23 +5,58 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClienteConAck {
-    private static final String SERVER_HOST = "localhost";
-    private static final int SERVER_PORT = 5000;
-
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket(SERVER_HOST, SERVER_PORT);
-        System.out.println("Conectado al servidor " + SERVER_HOST + ":" + SERVER_PORT);
 
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        // Dirección y puerto del servidor al que nos conectamos
+        String servidor = "localhost";
+        int puerto = 5000;
 
-       
-            String message = "Hola servidor!";
-            out.println(message);
-            String msjRecibido = in.readLine();
-            System.out.println(msjRecibido);
-            if (msjRecibido != null)
-                out.println("ACK");
-            socket.close();
+        // Creamos un socket para conectarnos al servidor
+        Socket socket = new Socket(servidor, puerto);
+
+        // Obtenemos los streams de entrada y salida para comunicarnos con el servidor
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+        // Pedimos al usuario que ingrese su nombre de usuario
+        System.out.println(input.readLine());
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        String nombreUsuario = teclado.readLine();
+        output.println(nombreUsuario);
+
+        // Ciclo principal del cliente
+        while (true) {
+            // Mostramos las opciones disponibles y pedimos al usuario que seleccione una
+            System.out.println(input.readLine());
+            String opcion = teclado.readLine();
+            output.println(opcion);
+
+            switch (opcion) {
+                case "1":
+                    // Pedimos al usuario que ingrese el nombre del destinatario y el mensaje
+                    System.out.println(input.readLine());
+                    String destinatario = teclado.readLine();
+                    output.println(destinatario);
+                    System.out.println(input.readLine());
+                    String mensaje = teclado.readLine();
+                    output.println(mensaje);
+
+                    // Mostramos la respuesta del servidor
+                    System.out.println(input.readLine());
+                    break;
+
+                case "2":
+                    // Mostramos los mensajes del usuario y esperamos a que presione Enter
+                    System.out.println(input.readLine());
+                    System.out.println(input.readLine());
+                    break;
+
+                case "3":
+                    // Cerramos la conexión con el servidor y salimos del programa
+                    socket.close();
+                    System.exit(0);
+                    break;
+            }
+        }
     }
 }
--- TP 1/Ejercicio 4/ej-4/src/main/java/ServidorConACK.java
+++ TP 1/Ejercicio 4/ej-4/src/main/java/ServidorConACK.java
@@ -1,57 +1,108 @@
-
-import java.io.*;
-import java.net.*;
-import java.util.concurrent.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 
 public class ServidorConACK {
-    private static final int PUERTO = 5000;
-    private static final int MAX_CLIENTES = 10;
-    private static final int MAX_MENSAJES = 100;
+
+    // Mapa que almacena las colas de mensajes de cada usuario
+    private static Map<String, Queue<String>> colasMensajes = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-        // Crea una cola para almacenar los mensajes
-        BlockingQueue<String> colaMensajes = new ArrayBlockingQueue<>(MAX_MENSAJES);
 
-        try (ServerSocket servidorSocket = new ServerSocket(PUERTO)) {
-            System.out.println("Servidor de colas iniciado en el puerto " + PUERTO);
+        // Puerto en el que el servidor espera conexiones de clientes
+        int puerto = 5000;
 
-            // Inicia un hilo para procesar cada cliente
-            ExecutorService executor = Executors.newFixedThreadPool(MAX_CLIENTES);
-            while (true) {
-                Socket clienteSocket = servidorSocket.accept();
-                executor.execute(() -> procesarCliente(clienteSocket, colaMensajes));
-            }
-        }
-    }
+        // Creamos un ServerSocket que escucha en el puerto indicado
+        ServerSocket serverSocket = new ServerSocket(puerto);
+        System.out.println("Servidor iniciado en el puerto " + puerto);
 
-    private static void procesarCliente(Socket clienteSocket, BlockingQueue<String> colaMensajes) {
-        try (BufferedReader entrada = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
-             PrintWriter salida = new PrintWriter(clienteSocket.getOutputStream(), true)) {
+        while (true) {
+            // Esperamos a que llegue una conexión de un cliente
+            Socket socket = serverSocket.accept();
 
-            // Espera por un mensaje del cliente
-            String mensaje = entrada.readLine();
-            System.out.println("Recibido mensaje: " + mensaje);
+            // Creamos un nuevo hilo para manejar la conexión con el cliente
+            Thread thread = new Thread(() -> {
+                try {
+                    System.out.println("Cliente conectado desde la dirección " + socket.getInetAddress());
 
-            // Agrega el mensaje a la cola
-            colaMensajes.put(mensaje);
+                    // Obtenemos los streams de entrada y salida para comunicarnos con el cliente
+                    BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+                    // Pedimos al cliente que ingrese su nombre de usuario
+                    output.println("Bienvenido al servidor de mensajes. Por favor, ingrese su nombre de usuario:");
+                    String nombreUsuario = input.readLine();
+
+                    // Verificamos si el usuario ya tiene una cola de mensajes
+                    if (!colasMensajes.containsKey(nombreUsuario)) {
+                        colasMensajes.put(nombreUsuario, new LinkedList<>());
+                    }
+
+                    // Ciclo principal del cliente
+                    while (true) {
+                        // Pedimos al cliente que seleccione una opción
+                        output.println("Seleccione una opción: 1. Dejar mensaje 2. Ver mensajes 3. Salir");
+                        String opcion = input.readLine();
+
+                        switch (opcion) {
+                            case "1":
+                                // Pedimos al cliente que ingrese el nombre del destinatario y el mensaje
+                                output.println("Ingrese el nombre del destinatario:");
+                                String destinatario = input.readLine();
+                                output.println("Ingrese el mensaje:");
+                                String mensaje = input.readLine();
+
+                                // Verificamos si el destinatario existe y dejamos el mensaje en su cola
+                                if (colasMensajes.containsKey(destinatario)) {
+                                    colasMensajes.get(destinatario).offer(nombreUsuario + ": " + mensaje);
+                                    output.println("Mensaje enviado correctamente.");
+                                } else {
+                                    output.println("El usuario " + destinatario + " no existe.");
+                                }
+                                break;
+
+                                case "2":
+                                // Mostramos los mensajes en la cola del usuario y los eliminamos
+                                Queue<String> colaMensajes = colasMensajes.get(nombreUsuario);
+                                if (!colaMensajes.isEmpty()) {
+                                    output.println("Mensajes para " + nombreUsuario + ":");
+                                    while (!colaMensajes.isEmpty()) {
+                                        output.println(colaMensajes.poll());
+                                    }
+                                    output.println("Por favor confirme que ha recibido los mensajes (escriba ack)");
+                                    String respuesta = input.readLine();
+                                    if(respuesta.equalsIgnoreCase("ack")){
+                                        // Borramos los mensajes de la cola
+                                        colasMensajes.remove(nombreUsuario);
+                                        colasMensajes.put(nombreUsuario, new LinkedList<>());
+                                        System.out.println("Mensajes eliminados");
+                                    }
+                                } else {
+                                    output.println("No tiene mensajes nuevos.");
+                                }
+                                break;
+
+                            case "3":
+                                // Cerramos la conexión con el cliente
+                                socket.close();
+                                System.out.println("Cliente desconectado."); 
+                        }
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error al comunicarse con el cliente.");
+                }
+            });
             
-            salida.println(mensaje);
-
-            // Espera por un ACK del cliente
-            String ack = entrada.readLine();
-            if ("ACK".equals(ack)) {
-                // Si el ACK es recibido, remueve el mensaje de la cola
-                colaMensajes.remove(mensaje);
-                System.out.println("Mensaje removido de la cola: " + mensaje);
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                clienteSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            thread.start();
         }
     }
 }
+                    
+                        
