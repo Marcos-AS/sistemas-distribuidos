bd8557381c7efbe0272a35a2ad5a6a15ab290e21
leoduville <71992403+leoduville@users.noreply.github.com>
2023-05-23 18:27:14 -0300

    actualizo

--- TP 2/Ejercicio 1/extreme/src/main/java/spring/extreme/controller/ExtremeServerController.java
+++ TP 2/Ejercicio 1/extreme/src/main/java/spring/extreme/controller/ExtremeServerController.java
@@ -1,43 +1,15 @@
 package spring.extreme.controller;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
-import jakarta.servlet.http.HttpServletResponse;
 import spring.extreme.services.Extremo;
 import spring.extreme.services.MaestroRequest;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 
 @RestController
 @RequestMapping("/extremo")
@@ -45,10 +17,7 @@ public class ExtremeServerController {
 
     @Autowired
     private Extremo extremo;
-
-    //@Value("${server.port}")
-    //private String numPuerto;
-        
+    
     @PostMapping("/informar")
     public void informar(@RequestBody MaestroRequest maestroRequest) {
         extremo.informarMaestro(maestroRequest.getDireccionIp(), maestroRequest.getPuerto());
@@ -64,11 +33,3 @@ public class ExtremeServerController {
         return extremo.enviarArchivo(archivo);
     }
 }
-
-
-
-
-
-    
-
-
