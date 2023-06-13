b3bc5f5624ed403444308fd79326488ea0781a74
leoduville <71992403+leoduville@users.noreply.github.com>
2023-06-13 12:27:45 -0300

    subo pero faltan hacer cosas

--- TP 2/Ejercicio 2/ejercicio b/demo/runnerGet.sh
+++ TP 2/Ejercicio 2/ejercicio b/demo/runnerGet.sh
@@ -1,2 +1,2 @@
 #curl -X GET -F "nombreImagen=image.jpg"  http://localhost:8080/unified-image --output imagen.jpg
-curl -X GET -F "nombreImagen=leo.jpg"  http://localhost:5051/unified-image --output imgs/messi.jpg
+curl -X GET -F "nombreImagen=leo.jpg"  http://34.72.249.225:8080/unified-image --output imgs/messi.jpg
