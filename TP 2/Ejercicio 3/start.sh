7a49904183c4d8abef1e54dcc9e0fe818fcd9330
Marcos-AS <54771734+Marcos-AS@users.noreply.github.com>
2023-12-01 09:55:22 -0300

    start.sh

---
+++ TP 2/Ejercicio 3/start.sh
@@ -0,0 +1,15 @@
+gcloud container clusters get-credentials primary --zone us-central1-a --project firm-source-399022
+cd mariadb/k8s
+kubectl apply -f deploy-bd.yaml
+kubectl apply -f service-bd.yaml
+cd ../../rabbitmq/k8s
+kubectl apply -f deploy-rabbit.yaml
+kubectl apply -f service-rabbit.yaml
+cd ../../Ejercicio\ 7/k8s
+kubectl apply -f deploy-webserver.yaml
+kubectl apply -f service-load-balancer.yaml
+cd ../../prueba/k8s
+kubectl apply -f deploy-worker.yaml
+kubectl apply -f service-worker.yaml
+kubectl get pod
+kubectl get service #get external-ip
